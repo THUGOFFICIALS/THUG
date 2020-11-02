ce59b171e75751522c29542e16afb1174793bfc5
THUG <73270173+THUGOFFICIALS@users.noreply.github.com>
2020-11-02 12:20:08 +0530

    Nikal

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use Mine\n Check @HellBot_Official For The Source!"
+            reply_pop_up_alert = "THIS IS PRIVATE BOT MAINTAINED BY THUG"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -85,7 +85,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     out_file,
                     force_document=True,
                     allow_cache=False,
-                    caption="Uploaded By @HellBot_Official"
+                    caption="Uploaded By THUGS "
                 )
 
 
